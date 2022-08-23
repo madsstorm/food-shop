e95a677f26c147aa9a323c582f24c114a70be294
Mads Storm Hansen <madsstorm@gmail.com>
2022-08-23 13:04:51 +0200

    allergens list

--- product-types.tf
+++ product-types.tf
@@ -3,19 +3,87 @@ resource "commercetools_product_type" "food" {
   description = "Food"
   key         = "food"
 
-  # attribute {
-  #   name = "Allergens"
-  #   label = {
-  #     en = "Allergens"
-  #   }
-  #   required   = false
-  #   constraint = "SameForAll"
-  #   searchable = true
-  #   type {
-  #     name = "set"
-  #     element_type {
-  #       name = "ltext"
-  #     }
-  #   }
-  # }
+  attribute {
+    name = "Allergens"
+    label = {
+      en = "Allergens"
+    }
+    required   = false
+    constraint = "SameForAll"
+    searchable = true
+    type {
+      name = "set"
+      element_type {
+        name = "enum"
+        value {
+          key = "wheat"
+          label = "Wheat"
+        }
+        value {
+          key = "barley"
+          label = "Barley"
+        }
+        value {
+          key = "oats"
+          label = "Oats"
+        }
+        value {
+          key = "rye"
+          label = "Rye"
+        }
+        value {
+          key = "crustaceans"
+          label = "Crustaceans"
+        }
+        value {
+          key = "celery"
+          label = "Celery"
+        }
+        value {
+          key = "eggs"
+          label = "Eggs"
+        }
+        value {
+          key = "fish"
+          label = "Fish"
+        }
+        value {
+          key = "lupin"
+          label = "Lupin"
+        }
+        value {
+          key = "milk"
+          label = "Milk"
+        }
+        value {
+          key = "molluscs"
+          label = "Molluscs"
+        }
+        value {
+          key = "mustard"
+          label = "Mustard"
+        }
+        value {
+          key = "nuts"
+          label = "Nuts"
+        }
+        value {
+          key = "mustard"
+          label = "Mustard"
+        }
+        value {
+          key = "peanut"
+          label = "Peanut"
+        }
+        value {
+          key = "sesame"
+          label = "Sesame"
+        }
+        value {
+          key = "soybean"
+          label = "Soybean"
+        }
+      }
+    }
+  }
 }
